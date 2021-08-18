526e0c9923ca19645fffd2a3fb60d010ad364ff3
357407741 <357407741@qq.com>
2021-08-18 17:47:03 +0800

    go

--- LGB/multi_class.py
+++ LGB/multi_class.py
@@ -1,21 +1,36 @@
+from lightgbm.engine import train
 import pandas as pd
 import numpy as np
 import lightgbm as lgb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 import gc
 
 ## load data
-train_data = pd.read_csv('LGB/data/train.csv')#读取训练集
-test_data = pd.read_csv('LGB/data/test.csv')
+data = pd.read_csv('LGB/data/labelresult1.csv')#读取数据
+# print(data.columns)
+
+del_lable = ['label']
+features = [i for i in data.columns if i not in del_lable]
+cate_feature = features
+# print(features)
+X_data = data[features]
+y_data = data['label'].astype(int)
+# print(y_data)
+xtrain,xtest,ytrain,ytest=train_test_split(X_data,y_data,train_size=0.85,random_state=2021)
+train=pd.concat([xtrain,ytrain])
+test=pd.concat([xtest,ytest])
+# print(xtrain.shape,xtest.shape,ytrain.shape,ytest.shape)
+
 num_round = 1000
 
 ## category feature one_hot
 # test_data['label'] = -1
 # data = pd.concat([train_data, test_data])
-cate_feature = ['gender', 'cell_province', 'id_province', 'id_city', 'rate', 'term']
+# cate_feature = ['gender', 'cell_province', 'id_province', 'id_city', 'rate', 'term']
 # for item in cate_feature:
 #     data[item] = LabelEncoder().fit_transform(data[item])
 
@@ -23,22 +38,17 @@ cate_feature = ['gender', 'cell_province', 'id_province', 'id_city', 'rate', 'te
 # test = data[data['label'] == -1]
 
 ##Clean up the memory
-del  train_data, test_data
+del  data
 gc.collect()
 
 ## get train feature
-del_feature = ['auditing_date', 'due_date', 'label']
-features = [i for i in train_data.columns if i not in del_feature]
 
-train_x = train_data[features]
-train_y = train_data['label'].astype(int)
-test = test_data[features]
 
 
 params = {'num_leaves': 60,
           'min_data_in_leaf': 30,
           'objective': 'multiclass',
-          'num_class': 33,#类别数
+          'num_class':49,#类别数
           'max_depth': -1,
           'learning_rate': 0.03,
           "min_sum_hessian_in_leaf": 6,
@@ -51,21 +61,21 @@ params = {'num_leaves': 60,
           "verbosity": -1,
           "nthread": 15,
           'metric': 'multi_logloss',
-          "random_state": 2019,
+          "random_state": 2021,
           # 'device': 'gpu' 
           }
 
 
 folds = KFold(n_splits=5, shuffle=True, random_state=2019)
-prob_oof = np.zeros((train_x.shape[0], 33))
-test_pred_prob = np.zeros((test.shape[0], 33))
+prob_oof = np.zeros((xtrain.shape[0], 49))
+test_pred_prob = np.zeros((xtest.shape[0], 49))
 
 ## train and predict
 feature_importance_df = pd.DataFrame()
-for fold_, (trn_idx, val_idx) in enumerate(folds.split(train_data)):
+for fold_, (trn_idx, val_idx) in enumerate(folds.split(train)):
     print("fold {}".format(fold_ + 1))
-    trn_data = lgb.Dataset(train_x.iloc[trn_idx], label=train_y.iloc[trn_idx])
-    val_data = lgb.Dataset(train_x.iloc[val_idx], label=train_y.iloc[val_idx])
+    trn_data = lgb.Dataset(xtrain.iloc[trn_idx], label=ytrain.iloc[trn_idx])
+    val_data = lgb.Dataset(xtrain.iloc[val_idx], label=ytrain.iloc[val_idx])
 
     clf = lgb.train(params,
                     trn_data,
@@ -74,7 +84,7 @@ for fold_, (trn_idx, val_idx) in enumerate(folds.split(train_data)):
                     verbose_eval=20,
                     categorical_feature=cate_feature,
                     early_stopping_rounds=60)
-    prob_oof[val_idx] = clf.predict(train_x.iloc[val_idx], num_iteration=clf.best_iteration)
+    prob_oof[val_idx] = clf.predict(xtrain.iloc[val_idx], num_iteration=clf.best_iteration)
 
 
     fold_importance_df = pd.DataFrame()
